e0a722c94c86d4fd7f677bbb46712ba4a4da96ea
taassburak <56767932+taassburak@users.noreply.github.com>
2020-07-02 14:14:03 +0300

    staj

---
+++ staj2dgame/Assets/scripts/karakter_kontrol.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class karakter_kontrol : MonoBehaviour
+{
+
+    public GameObject range;
+
+    public Sprite[] beklemeAnim;
+    public Sprite[] ziplamaAnim;
+    public Sprite[] yurumeAnim;
+    public Sprite[] olumAnim;
+    public Sprite[] attackAnim;
+
+
+    
+    int olumAnimSayac = 0;
+    int beklemeAnimSayac = 0;
+    int ziplamaAnimSayac = 0;
+    int yurumeAnimSayac = 0;
+    int attackAnimSayac = 0;
+
+    SpriteRenderer spriteRenderer;
+    float horizontal = 0;
+    float waitingTime = 0;
+    float runningTime = 0;
+    float olumTime = 0;
+    float attackTime = 0;
+    Rigidbody2D physics;
+    Vector3 vec;
+    bool jumpeonce = true;
+    GameObject kamera;
+
+    Vector3 kameraSonPos;
+    Vector3 kameraIlkPos;
+
+    int can = 100;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        physics = GetComponent<Rigidbody2D>();
+
+        //kameranın oyuncuyu takip etmesi için bir gameobject oluşturup main kamerayı oluşturduğumuz kamera objesine verdik
+        kamera = GameObject.FindGameObjectWithTag("MainCamera");
+        kameraIlkPos = kamera.transform.position - transform.position;
+    }
+
+    
+    void Update()
+    {
+        Attack();
+        //ikinci defa zıplamaması için
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpeonce && can != 0)
+            {
+                physics.AddForce(new Vector2(0, 100));
+                jumpeonce = false;
+            }
+
+
+        }
+    }
+    void FixedUpdate()
+    {
+        Animation();
+        charmove();
+        
+    }
+    void charmove()
+    {
+        // karakteri yürütmek için
+        if (can != 0)
+        {
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vec = new Vector3(horizontal * 2, physics.velocity.y, 0);
+            physics.velocity = vec;
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //updatede yazdığım kodun çalışması için collision enter verdim
+        jumpeonce = true;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag=="dusman")
+        {
+            can = 0;
+        }  
+    }
+
+    void Animation()
+    {
+        if (jumpeonce == true)
+        {
+            if (horizontal == 0 && can!=0 )
+            {
+                waitingTime += Time.deltaTime;
+                if (waitingTime > 0.09f)
+                {
+                    spriteRenderer.sprite = beklemeAnim[beklemeAnimSayac++];
+                    if (beklemeAnimSayac == beklemeAnim.Length)
+                    {
+                        beklemeAnimSayac = 0;
+                    }
+                    waitingTime = 0;
+                }
+
+            }
+            else if (horizontal > 0 && can != 0)
+            {
+                runningTime += Time.deltaTime;
+                if (runningTime > 0.1f)
+                {
+                    spriteRenderer.sprite = yurumeAnim[yurumeAnimSayac++];
+                    if (yurumeAnimSayac == yurumeAnim.Length)
+                    {
+                        yurumeAnimSayac = 0;
+                    }
+                    runningTime = 0;
+                }
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (horizontal < 0 && can != 0)
+            {
+                runningTime += Time.deltaTime;
+                if (runningTime > 0.1f)
+                {
+                    spriteRenderer.sprite = yurumeAnim[yurumeAnimSayac++];
+                    if (yurumeAnimSayac == yurumeAnim.Length)
+                    {
+                        yurumeAnimSayac = 0;
+                    }
+                    runningTime = 0;
+                }
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+
+        else if (jumpeonce == false  )
+        {
+                spriteRenderer.sprite = ziplamaAnim[0];
+
+            if (horizontal > 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (horizontal < 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+
+        if (can == 0)
+        {
+            olumTime += Time.deltaTime;
+            if (olumTime > 0.05f)
+            {
+                spriteRenderer.sprite = olumAnim[olumAnimSayac++];
+                if (olumAnimSayac == olumAnim.Length)
+                {
+                    olumAnimSayac = 0;
+                }
+                olumTime = 0;
+             
+            }
+        }
+    }
+    void Attack()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            range.gameObject.SetActive(true);
+            attackTime += Time.deltaTime;
+            if (attackTime > 0.001f)
+            {
+                spriteRenderer.sprite = attackAnim[attackAnimSayac++];
+                if (attackAnimSayac == attackAnim.Length)
+                {
+                    attackAnimSayac = 0;
+                }
+                attackTime = 0;
+            }
+        }
+        else
+            range.gameObject.SetActive(false);
+    }
+
+    void kameraKontrol()
+    {
+        kameraSonPos = kameraIlkPos + transform.position;
+        //kamera.transform.position = Vector3.Lerp(kamera.transform.position, kameraSonPos, 0.5f);
+        kamera.transform.position = kameraSonPos;
+    }
+}
---
+++ staj2dgame/Assets/scripts/zombie_kontrol.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie_kontrol : MonoBehaviour
+{
+
+    public Sprite[] beklemeAnim;
+    float beklemeAnimTime = 0;
+    int beklemeAnimSayac = 0;
+
+    
+
+    public Sprite[] olumAnim;
+    float olumAnimTime = 0;
+    int olumAnimSayac = 0;
+
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D fizik;
+
+    public int ENcan = 100;
+
+
+    GameObject zombiparts;
+   
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        fizik = GetComponent<Rigidbody2D>();
+        zombiparts = GameObject.Find("zombie_parts");
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(ENcan);
+    }
+    private void FixedUpdate()
+    {
+        animasyon();
+       
+    }
+
+    void animasyon()
+    {
+
+        beklemeAnimTime += Time.deltaTime;
+        if (beklemeAnimTime > 0.09f)
+        {
+            spriteRenderer.sprite = beklemeAnim[beklemeAnimSayac++];
+            if (beklemeAnimSayac == beklemeAnim.Length)
+            {
+                beklemeAnimSayac = 0;
+            }
+            beklemeAnimTime = 0;
+        }
+
+        if (ENcan==0)
+        {
+            /*gameObject.GetComponent<SpriteRenderer>().enabled=false;
+
+            for (int i = 0; i < zombiparts.transform.childCount; i++)
+            {
+                zombiparts.transform.GetChild(i).gameObject.SetActive(true);
+                
+                
+            }*/
+        }
+
+    }
+
+    
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "sword")
+        {
+            ENcan -= 20;
+
+        }
+    }
+}
